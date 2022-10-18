fcca40a7ece284e597f0f5d6442f83ceca5bddb2
Griga178 <grisha.tis@gmail.com>
2022-10-18 17:41:29 +0300

    JPG VIEWR HTML TEST result2

--- html_css_js_test/js/start_change.js
+++ html_css_js_test/js/start_change.js
@@ -1,21 +1,43 @@
-let result_list = [1, 2, 3]
+// ДАННЫЕ ОТ СЕРВЕРА
+let list_jpg_id = ['3', '4', '5', '6', '8']
+let list_name_value = ['Пневматический очиститель', 'Баллон со сжатым воздухом', 'Пневматический очиститель', 'Пневматический очиститель', 'Коврик для мыши']
+let list_price_value = [269, 334.53, 304, 181, 209]
 
 button_next.onclick = () => {
-  alert(screen_shot.textContent)
+  let cur_i = GetIndexByScreenID(PScreenID.textContent)
+  let data_arr = GetDataByIndex(cur_i + 1)
+  FillResultBlock(data_arr)
 }
 
 button_previous.onclick = () => {
-  alert(screen_shot.textContent)
+  let cur_i = GetIndexByScreenID(PScreenID.textContent)
+  let data_arr = GetDataByIndex(cur_i - 1)
+  FillResultBlock(data_arr)
 }
 
 window.addEventListener("keydown", function(e) {
   if (e.key == "ArrowLeft") {
-    button_next.click();
+    button_previous.click();
   }
 });
 
 window.addEventListener("keydown", function(e) {
   if (e.key == "ArrowRight") {
-    button_previous.click();
+    button_next.click();
   }
 });
+
+function FillResultBlock ([jpg_id, name_value, price_value]) {
+  PScreenID.innerHTML = jpg_id
+  CurrentImageContainer.setAttribute('src', 'images/' + jpg_id + '.jpg')
+  SpanTextName.innerHTML = name_value
+  SpanPriceValue.innerHTML = price_value
+}
+// ИНДЕС В СПИСКЕ ПО НАЗВАНИЮ СКРИНА
+function GetIndexByScreenID(screen_id) {
+  return list_jpg_id.indexOf(screen_id)
+}
+// ИНФА ИЗ ОТВЕТА СЕРВЕРА ПО ИНДЕКСУ
+function GetDataByIndex(scr_index){
+  return [list_jpg_id[scr_index], list_name_value[scr_index], list_price_value[scr_index]]
+}
