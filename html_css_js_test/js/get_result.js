49261055fadc9322c9221f4b292a919da9981a85
Griga178 <grisha.tis@gmail.com>
2022-10-19 21:49:54 +0300

    result description

--- html_css_js_test/js/get_result.js
+++ html_css_js_test/js/get_result.js
@@ -11,10 +11,8 @@
 // │   │   ├── 'check_status': bool
 
 
-let results_from_srv = [
-  {
-    "result_id": 3
-    "parsing_date": '22.08.2022',
+let results_from_srv = [{
+    "result_id": 3 "parsing_date": '22.08.2022',
     'link': 'https://citilink.ru',
     'domain_name': 'citilink.ru',
     'product_name': 'Пневматический очиститель',
@@ -23,8 +21,7 @@ let results_from_srv = [
     "check_status": false
   },
   {
-    "result_id": 4
-    "parsing_date": '22.09.2022',
+    "result_id": 4 "parsing_date": '22.09.2022',
     'link': 'https://google.ru',
     'domain_name': 'dns.ru',
     'product_name': 'Баллон со сжатым воздухом',
@@ -33,8 +30,7 @@ let results_from_srv = [
     "check_status": false
   },
   {
-    "result_id": 5
-    "parsing_date": '22.09.2022',
+    "result_id": 5 "parsing_date": '22.09.2022',
     'link': 'https://google.ru',
     'domain_name': 'dns.ru',
     'product_name': 'Пневматический очиститель',
@@ -43,8 +39,7 @@ let results_from_srv = [
     "check_status": false
   },
   {
-    "result_id": 6
-    "parsing_date": '22.08.2022',
+    "result_id": 6 "parsing_date": '22.08.2022',
     'link': 'https://google.ru',
     'domain_name': 'citilink.ru',
     'product_name': 'Пневматический очиститель',
@@ -53,8 +48,7 @@ let results_from_srv = [
     "check_status": false
   },
   {
-    "result_id": 8
-    "parsing_date": '22.08.2022',
+    "result_id": 8 "parsing_date": '22.08.2022',
     'link': 'https://google.ru',
     'domain_name': 'citilink.ru',
     'product_name': 'Коврик для мыши',
@@ -70,51 +64,12 @@ let results_from_srv = [
 // │   ├──  date_start: str def = 0
 // │   ├──  date_end: str def = 0
 
-function get_result(filtered_qery){
+function get_result(filtered_qery) {
 
   return results_from_srv
 }
 
-function create_query(){
+function create_query() {
   let query_to_srv = {}
   return query_to_srv
 }
-
-// 1 пользователь открывает РЕЗУЛЬТАТЫ
-// 2 клиент запрашивает данные у сервера для заполнения ФИЛЬТРА
-// 3 клиент получает список для ФИЛЬТРА заполняет ЕГО
-// 4 пользователь в ФИЛЬТРЕ формирует запрос для поиска РЕЗУЛЬТАТОВ
-// 5 клиент делает запрос получаает список РЕЗУЛЬТАТОВ
-// 6 клиент разделяет список Р. на части возвращает список списков
-// 7 клиент создает генератор списка
-// 8 клиент запрашивает данные РЕЗУЛЬТАТОВ по списку ID
-// 9 клиент заполняет блоки РЕЗУЛЬТАТ + СПИСОК
-// 10 пользователь пролистывает РЕЗУЛЬТАТЫ
-// 11 клиент на каждом результате меняет статус - "Проверен - 2"
-// 12 клиент на выбранных результатах меняет цены
-// 13 пользователь долистал до конца
-// 14 клиент обращается к функции с итератором === 14
-// 15 клиент запрашывает у сервера данные по второй части === 15
-// 16 клиент очищает данные первой части результатов - (Повтор)
-// 17 клиент вызывает событие об окончании списка
-
-// 1 -
-// 2 - getDataForFilter() --> filterData {}
-// 3 - fillFilterBlock(filterData)
-// 4 - readFilteredQuery() --> filteredQuery {}
-// 5 - getResultInfo(filteredQuery) --> srvListResponce []
-// 6 - separateResponceList(srvListResponce) --> separatedList [ [],... ]
-// 7 - generateSequence(separatedList) *--> listPart []
-// 8 - getResulData(listPart) --> resultData {}
-// 9 - fillResultBlock(resultData)
-
-
-// 1 - активируется пользователем
-// 2 - прописана в HTML
-// 3 - активируется ф-ей № 2
-// 4 - активируется пользователем
-// 5 - активируется ф-ей № 4
-// 6 - активируется ф-ей № 5
-// 7* - активируется ф-ей № 6 генератор
-// 8 - активируется ф-ей* № 7
-// 9 - активируется ф-ей № 8
