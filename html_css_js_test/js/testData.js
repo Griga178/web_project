2e8c2c43b501e73a375ac8ef12ab0ff00000e77b
Griga178 <grisha.tis@gmail.com>
2022-10-20 17:40:38 +0300

    result - query filter is ready

---
+++ html_css_js_test/js/testData.js
@@ -0,0 +1,10 @@
+let filterData = {
+  "domain_list": [{"id":1, "domain_name": "citilink.ru"},
+                {"id":2, "domain_name": "dns.ru", 'result_amount': 1},
+                {"id":3, "domain_name": "xcom.ru", 'result_amount': 1},
+                {"id":4, "domain_name": "onlinetrade.ru", 'result_amount': 1},
+                {"id":5, "domain_name": "computermarket.ru", 'result_amount': 1},
+                {"id":6, "domain_name": "delo.spb.ru", 'result_amount': 1}],
+  "date_start": "2022-08-22",
+  "date_end": "2022-09-05"
+}
